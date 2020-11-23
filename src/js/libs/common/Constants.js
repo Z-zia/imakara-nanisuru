d6401bb6be122c48897fce560d4333e5791bc216
zia <ziasan@hotmail.co.jp>
2020-11-23 15:13:52 +0900

    2020/11/23 更新

---
+++ src/js/libs/common/Constants.js
@@ -0,0 +1,7 @@
+export default class Constants {
+  static get LOCAL_STORAGE_USER_KEY() { return 'LOCAL_STORAGE_USER_KEY'; }
+  static get LINK_POCHIPOCHI() { return '/pochipochi'; }
+  static get LINK_SETTING_NORMAL() { return '/setting/normal'; }
+  static get LINK_SETTING_TODO() { return '/setting/todo'; }
+  static get LINK_SETTING_TOSTOP() { return '/setting/tostop'; }
+}
---
+++ src/js/libs/common/ReduxConstants.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default class ReduxConstants {
+  static get SET_USER() { return 'SET_USER'; }
+  static get UPDATE_TOKEN() { return 'UPDATE_TOKEN'; }
+  static get UPDATE_TOKEN_ASYNC() { return 'UPDATE_TOKEN_ASYNC'; }
+  static get REDIRECT() { return 'REDIRECT'; }
+  static get LOADING() { return 'LOADING'; }
+  static get LOADED() { return 'LOADED'; }
+  static get FETCH_WEIGHT_FULFILLED() { return 'FETCH_WEIGHT_FULFILLED'; }
+  static get FETCH_WEIGHT_REJECTED() { return 'FETCH_WEIGHT_REJECTED'; }
+  static get RELOAD_TODAYS_WEIGHT() { return 'RELOAD_TODAYS_WEIGHT'; }
+  static get RELOAD_ONE_WEEKS_WEIGHT() { return 'RELOAD_ONE_WEEKS_WEIGHT'; }
+  static get RELOAD_ONE_MONTH_WEIGHT() { return 'RELOAD_ONE_MONTH_WEIGHT'; }
+  static get RELOAD_ONE_YEAR_WEIGHT() { return 'RELOAD_ONE_YEAR_WEIGHT'; }
+  static get CHANGE_TODAYS_WEIGHT() { return 'CHANGE_TODAYS_WEIGHT'; }
+}
